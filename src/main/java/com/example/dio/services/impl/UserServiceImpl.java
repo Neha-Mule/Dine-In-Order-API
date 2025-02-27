a435333450089b2d1e40d97592dd4f1845d7a992
Neha-Mule <150320983+Neha-Mule@user.noreply.com>
2025-02-27 17:09:38 +0530

    commited the modified file

--- src/main/java/com/example/dio/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/dio/services/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.dio.services.impl;
 
+import com.example.dio.enums.UserRole;
+import com.example.dio.module.Admin;
+import com.example.dio.module.Staff;
 import com.example.dio.module.User;
 import com.example.dio.repository.UserRepository;
 import com.example.dio.services.UserService;
@@ -11,8 +14,33 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
     @Override
-    public User registerUser(User user) {
-        return userRepository.save(user);
+    public User registerUser(User user)
+    {
+        User user1 = createUserByRole(user.getUserRole());
+        this.mapToNewUser(user,user1);
+
+
+        return userRepository.save(user1);
+    }
+
+    private User createUserByRole(UserRole role){
+        User user;
+        switch (role){
+            case ADMIN -> user = new Admin();
+            case STAFF -> user = new Staff();
+            default -> throw new RuntimeException("Failed to register user");
+
+        }
+        return user;
+    }
+
+    private void mapToNewUser(User user,User user2){
+        user2.setUserName(user.getUserName());
+        user2.setEmail(user.getEmail());
+        user2.setPassword(user.getPassword());
+        user2.setUserRole(user.getUserRole());
+        user2.setPhoneNumber(user.getPhoneNumber());
     }
 }
