0c930845aaf162789a1ca84f9d6586790c2a613a
Neha-Mule <150320983+Neha-Mule@user.noreply.com>
2025-02-28 10:58:02 +0530

    created 2 features that is find user by id and update user details by id

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -7,9 +7,7 @@ import com.example.dio.util.ResponseStructure;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -18,12 +16,22 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<String>> registerUser(@RequestBody User user){
+    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user){
         user = userService.registerUser(user);
-
-        return ResponseBuilder.success(HttpStatus.CREATED,"Created User","user");
+        return ResponseBuilder.success(HttpStatus.CREATED,"Created User",user);
     }
 
+    @GetMapping("/users/{userid}")
+    public ResponseEntity<ResponseStructure<User>> findUserById(@PathVariable Long userid){
+        User user = userService.findUserById(userid);
+        return ResponseBuilder.success(HttpStatus.OK,"User Found",user);
+    }
+
+    @PutMapping("/update/{userid}")
+    public ResponseEntity<ResponseStructure<User>> updateUserById(@PathVariable Long userid,@RequestBody User user){
+        user = userService.updateUserById(userid,user);
+        return ResponseBuilder.success(HttpStatus.OK,"User Updated",user);
+    }
 
     }
 
